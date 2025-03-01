bc041748bd7febff898e382a9b526534f18bc2b3
mouad771 <mouadeddib7@gmail.com>
2025-03-01 19:11:15 +0000

    count arae

---
+++ count_arae.py
@@ -0,0 +1,10 @@
+lenght = input("please type lenght: \n" )
+width = input("please type width: \n")
+metre = input("how much for 1m etre: \n")
+float_lenght= float(lenght)
+float_width= float(width)
+float_metre=float(metre)
+arae_= float_lenght * float_width
+print("arae is: m", arae_)
+totel_price= float_metre * arae_
+print("totel price is ; $", totel_price)
